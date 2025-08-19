e8d603afa80f660f623165eeff3b8e1ff1048a80
saqlain noorani <saqlainnoorani17@gmail.com>
2025-08-19 08:33:58 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,6 +79,8 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3271205527151622"
+     crossorigin="anonymous"></script>
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
